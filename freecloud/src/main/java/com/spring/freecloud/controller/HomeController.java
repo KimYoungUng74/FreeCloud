d4dc9f3a706f14bb000fadc531841abaef75a864
tk941012 <tk941012@naver.com>
2020-06-10 10:02:45 +0900

    Revert "Revert "프로젝트 등록 / DB 시퀀스 생성""
    This reverts commit c01568854116009ccd7de54dec223b38d11ae062.

--- freecloud/src/main/java/com/spring/freecloud/controller/HomeController.java
+++ freecloud/src/main/java/com/spring/freecloud/controller/HomeController.java
@@ -45,7 +45,7 @@ public class HomeController {
 
 		model.addAttribute("serverTime", formattedDate );
 		
-		return "rewardReg";
+		return "projectReg";
 	}
 	
 	@RequestMapping(value = "home.do", method = RequestMethod.GET)
